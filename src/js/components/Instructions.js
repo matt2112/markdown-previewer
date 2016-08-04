f7ac2751062764399001bbebc1d420be3cd22f33
matt2112 <matt_lewis@live.co.uk>
2016-08-04 11:46:58 +0100

    small bug fixes

--- src/js/components/Instructions.js
+++ src/js/components/Instructions.js
@@ -8,11 +8,11 @@ export default class Instructions extends React.Component {
                     <h2>Instructions</h2>
                     <ul>
                         <li>
-                            Type <a href="https://help.github.com/articles/about-writing-and-formatting-on-github/" target="blank">GitHub Flavored Markdown</a> into the Input box.
+                            Type <a href="https://help.github.com/articles/about-writing-and-formatting-on-github/" target="_blank">GitHub Flavored Markdown</a> into the Input box.
                             It will then be formatted and displayed in the Output box.
                         </li>
                         <li>For example, type _some text_ to display "some text" in italics, or __some text__ to display it in bold.</li>
-                        <li>See <a href="https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet" target="blank">here</a> for a full cheatsheet.</li>
+                        <li>See <a href="https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet" target="_blank">here</a> for a full cheatsheet.</li>
                     </ul>
                 </div>
             </div>
